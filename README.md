74a0ca87eacdf78b69c9b92dd312d01d9e0b3c79
Hashim Shafiq <Hashim Shafiq>
2016-11-20 09:11:24 +0500

    update 2

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
-# [Twitter Sentiment Analysis Visualization][site-url]
->Sentiment Analysis of Twitter Accounts
 
-![](screenshot.png)
-
-[site-url]: "https://tsa-webapp.herokuapp.com/"
--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 var config = {
-  consumer_key: 'JM47nKk98JbFN4fgsGZUsc1jP',
-  consumer_secret: 'ILSoVTxITYrukszqSToe7t0SUY1wo2beXrMRwHPimkanv97YHb',
-  access_token_key: '172713864-oFRX7JhegfLCpN1LHHFvGUld6TWnjUAu699GDJj7',
-  access_token_secret: '9pCxUrwEc93TmMdnk0lVSqbVoNmcfaJ8qNb6TiXvzzRMA'
+  consumer_key: 'gCJIyjMgtADe3jN4HhUQCGktS',
+  consumer_secret: 'XNGOwzI7kQ4kHRQNwV02kfE3qVnQoWSecfz0hTca5PgmbhSo9s',
+  access_token_key: '1401379530-Go6ocdo3ScWZKtGAjpREZbZA8OHLYBX3z1a3Xtt',
+  access_token_secret: 'u7x0t8TjsS02vXVPeqyNCOLtKxRsHghTDX75i1bW8kjEq'
 }
 
 module.exports = config;
